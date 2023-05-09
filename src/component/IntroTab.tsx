9556858c298f7923935bc36ebff30d72f39aebd9
varun7882 <varunparas.srivastava8@gmail.com>
2023-05-09 12:26:42 +0530

    Adding intro tab

---
+++ src/component/IntroTab.tsx
@@ -0,0 +1,15 @@
+import { Row } from "react-bootstrap"
+export default function IntroTab() {
+    return (
+      <div className="Row">
+        <div className="col-md-4">
+            <img src="resource/dp.jpg" alt="this is me"></img>
+        </div>
+        <div className="col-md-8">
+            <p>
+                This is my description. Some guy trying to do some things. High in motivation so much so that is touches unrealism and naivity.
+            </p>
+        </div>
+      </div>
+    )
+  }
