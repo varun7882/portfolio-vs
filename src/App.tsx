4bff0f349440b0f0043df82f3b41d4f894066f7c
varun7882 <varunparas.srivastava8@gmail.com>
2023-05-13 17:16:19 +0530

    added stylesheets

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import IntroTab from './component/IntroTab';
 
 function App() {
   return (
+    <>
     <div className="App">
        I am varun srivastava
     </div>
+    <IntroTab/>
+    </>
   );
 }
 
